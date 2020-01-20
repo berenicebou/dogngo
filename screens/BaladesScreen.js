5637c523fd46d496e17ff5caecd073ae7b5495d5
Bérénice Boulade <b.berenice@orange.fr>
2020-01-21 00:29:07 +0100

    :sparkles: Squelette page balade

--- screens/BaladesScreen.js
+++ screens/BaladesScreen.js
@@ -1,20 +1,73 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Content } from 'native-base';
+import { ScrollView, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
+import { Text, Content, Thumbnail, Button, Card, CardItem, Left, Right, Body, ListItem, List} from 'native-base';
 
+import events from '../data/eventData'
+import EventItem from '../components/EventItem'
 
-export default function BaladesScreen() {
+class BaladesScreen extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  _displayDetailEvent = (idEvent) => {
+    console.log(idEvent)
+    this.props.navigation.navigate("EventDetail" , { idEvent: idEvent })
+  }
+  render(){
+  let {height, width} = Dimensions.get('window');   
   return (
-    <Content style={styles.container}>
+    <Content>
+      <Content>
+      <Card>
+            <CardItem>
+              <Left>
+                <Text>Balades effectués</Text>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <List>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+              </List>
+            </CardItem>
+
+            <CardItem>
+              <Left>
+                <Text>Balades effectués</Text>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <List>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+              </List>
+            </CardItem>
+
+
+          </Card>
+      </Content>
 
     </Content>
   );
 }
+}
+
+BaladesScreen.navigationOptions = {
+  title: "Mes balades",
+  headerStyle: {
+		marginTop:-50
+	}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor:'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
+
+export default BaladesScreen
