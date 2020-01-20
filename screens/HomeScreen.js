a68cfe6df2e6c325609ddd09c6a08c631526339e
Bérénice Boulade <b.berenice@orange.fr>
2020-01-20 21:53:29 +0100

    :sparkles: Floating Action Button Ajouter

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,7 +19,10 @@ import {
   Item,
   Thumbnail,
   Left,
-  Right, 
+  Right,
+  Card,
+  Fab, 
+  Icon
 } from 'native-base';
 import Colors from '../constants/Colors';
 import Texts from '../constants/Texts';
@@ -32,31 +35,41 @@ import AdItem from '../components/AdItem';
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
-  return (
-    <Content>
-        <Content style={{flex:1}}>
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    };
+  }
+  render() {
+    return (
+    <Container>
+      <Content>
 
+        {/* Partie pour écrire un post */}
+        {/* <Card style={{flex:1}} transparent>
           <Content>
-            <Item style={{borderBottomWidth:0}}>
+            <Item style={{borderBottomWidth:0, marginLeft:10}}>
               <Thumbnail source={require('../assets/images/avatar/avatar.jpg')}/>
               <Input placeholder="Ecrire quelque chose..." />
             </Item>
           </Content>
 
-          <Content style={{alignSelf:'flex-end'}}>
+          <Content style={{alignSelf:'flex-end', marginRight:10}}>
               <Button rounded style={{backgroundColor: Colors.buttonColor}}>
                 <Text>Poster</Text>
               </Button>
           </Content>
+        </Card> */}
 
+        {/* LES BALADES PROCHES */}
+        <Content>
+          <Text style={Texts.h1}>Les balades proches de moi</Text>
+          
         </Content>
 
-        <Content style={styles.sponso}>
-          <Text style={Texts.h1}>Les promos</Text>
-          <AdItem />
-        </Content>
-        
+        {/* ACTUALITES */}
         <Content>
           <Text style={Texts.h1}>Les actualités</Text>
           <FlatList
@@ -66,9 +79,39 @@ export default function HomeScreen() {
           />
         </Content>
 
-    </Content>
-        
-  );
+        {/* PROMO */}
+        <Content style={styles.sponso}>
+          <Text style={Texts.h1}>Les promos</Text>
+          <AdItem />
+        </Content>
+
+        </Content>
+
+        {/* Floating Action Button */}
+          <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ right:10, bottom:10 }}
+            style={{ backgroundColor:Colors.tintColor}}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            <Icon type="AntDesign" name="plus"/>
+            <Button style={{ backgroundColor: '#34A34F' }}>
+              <Icon name="logo-whatsapp" />
+            </Button>
+            <Button style={{ backgroundColor: '#3B5998' }}>
+              <Icon name="logo-facebook" />
+            </Button>
+            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+              <Icon name="mail" />
+            </Button>
+          </Fab>
+
+    </Container>
+    
+     
+    );
+  }
 }
 
 HomeScreen.navigationOptions = {
