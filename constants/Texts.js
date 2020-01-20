8d7a84623b4464599ec4b6c57174bf81106ad694
Bérénice Boulade <b.berenice@orange.fr>
2020-01-20 11:34:19 +0100

    :bento: Constantes text

--- constants/Texts.js
+++ constants/Texts.js
@@ -1,9 +1,15 @@
+import Colors from '../constants/Colors'
+
+
 export default {
     h1: {
         marginLeft:15,
         fontSize:20,
         fontWeight:'bold',
         marginBottom:0,
+   },
+   txtLight:{
+        color: Colors.tintColor,
    }
   };
   
