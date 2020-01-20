ee79ce7ba2e19df29ca311e0f9eaa1ed8607f2b4
Bérénice Boulade <b.berenice@orange.fr>
2020-01-21 00:38:52 +0100

    :poop: Nettoyage

--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import {Text} from 'native-base';
 
 export default function ProfilScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+    <Text>Profil</Text>
+  );
 }
 
 ProfilScreen.navigationOptions = {
-  title: 'app.json',
+  header: null,
 };
