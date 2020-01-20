ad3a89d39cfa7ea07c1421e3156b76d089cb4629
Bérénice Boulade <b.berenice@orange.fr>
2020-01-21 00:38:23 +0100

    :lipstick: Barre recherche carte

--- screens/CarteScreen.js
+++ screens/CarteScreen.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
-import { Text, Content, Thumbnail, Button } from 'native-base';
+import { Text, Content, Thumbnail, Button, Container, Header, Item, Icon, Input, Card } from 'native-base';
 
 import events from '../data/eventData'
 import EventItem from '../components/EventItem'
+
+import Colors from '../constants/Colors'
 
 class CarteScreen extends React.Component {
   constructor(props){
     super(props)
   }
-  _displayDetailEvent = (idEvent) => {
-    console.log(idEvent)
-    this.props.navigation.navigate("EventDetail" , { idEvent: idEvent })
+  _displayDetailEvent = (idEvent, titleEvent, typeEvent) => {
+    console.log(idEvent, titleEvent)
+    this.props.navigation.navigate("EventDetail" , { idEvent: idEvent, titleEvent: titleEvent, typeEvent :typeEvent })
   }
   render(){
   let {height, width} = Dimensions.get('window');   
   return (
     <Content>
+
+      <Card style={styles.searchBar}>
+          <Item>
+            <Input />
+            <Button style={{backgroundColor:Colors.tintColor}}>
+              <Text>Chercher</Text>
+            </Button>
+          </Item>
+    </Card>
       <Content>
       <FlatList
-            data={events}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({item}) => <EventItem event={item} displayDetailEvent={this._displayDetailEvent}/>}
-          />
+        data={events}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => <EventItem event={item} displayDetailEvent={this._displayDetailEvent}/>}  
+        />
       </Content>
-
+      
       <Image style={[styles.backgroundImage, {height:height, width: width}]} source={require('../assets/images/fakemap.png')}/>
-      <Thumbnail style={{position:'absolute', alignSelf:'center'}} source={require('../assets/images/icone-menu-balade2.png')}/>
     </Content>
   );
 }
@@ -38,10 +48,16 @@ CarteScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   backgroundImage: {
-    flex: 1,
     backgroundColor:'transparent',
     justifyContent: 'center',
+    flex: 1,
     alignItems: 'center',
+  },
+  marqueur:{
+    zIndex:2
+  },
+  searchBar:{
+    backgroundColor: 'transparent',
   }
 });
 
