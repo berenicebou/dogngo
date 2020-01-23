6da35c711c34e0aaf691c38be1d43bc9ab347abc
Bérénice Boulade <b.berenice@orange.fr>
2020-01-23 13:32:56 +0100

    :lipstick: UI du tab

--- screens/ProfilScreen/index.js
+++ screens/ProfilScreen/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import {Text, Content, Thumbnail, Card, CardItem, Tab, Tabs, Button} from 'native-base';
 
 import avatar from '../../data/avatarData'
@@ -8,14 +8,15 @@ import avatar from '../../data/avatarData'
 import TabMur from './tabMur'
 import TabChien from './tabChien'
 import TabPhotos from './tabPhoto'
+import Colors from '../../constants/Colors';
 
 export default class ProfilScreen extends React.Component {
   render(){
     return (
       <Content>
         <Content>
-              <Card style={{alignContent:'center', alignItems:'center'}} transparent>
-                <Thumbnail source={avatar.profile_pic}/>
+              <Card style={styles.profileHead} transparent>
+                <Thumbnail style={{width:80, height:80}} source={avatar.profile_pic}/>
                 <CardItem style={{flexDirection:'row'}}>
                   <Text>{avatar.prenom} {avatar.nom},</Text><Text> de {avatar.location}</Text>
                 </CardItem>
@@ -25,14 +26,14 @@ export default class ProfilScreen extends React.Component {
           </Button>
         </Content>
         <Content>
-          <Tabs>
-            <Tab heading='Mur'>
+          <Tabs tabBarUnderlineStyle={{backgroundColor:Colors.darkBlue}}>
+            <Tab heading='Mur' tabStyle={styles.tabColor} activeTabStyle={styles.tabColor} textStyle={styles.tabText} activeTextStyle={styles.tabText}>
              <TabMur avatar={avatar}/> 
             </Tab>
-            <Tab heading='Chien'>
+            <Tab heading='Chien' tabStyle={styles.tabColor} activeTabStyle={styles.tabColor} textStyle={styles.tabText} activeTextStyle={styles.tabText}>
               <TabChien chien={avatar.chien}/>
             </Tab>
-            <Tab heading='Photos'>
+            <Tab heading='Photos' tabStyle={styles.tabColor} activeTabStyle={styles.tabColor} textStyle={styles.tabText} activeTextStyle={styles.tabText}>
               <TabPhotos photos={avatar.photos}/>
             </Tab>
           </Tabs>
@@ -42,6 +43,21 @@ export default class ProfilScreen extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({
+  profileHead:{
+    alignContent:'center',
+    alignItems:'center',
+    height:150
+  },
+  tabColor:{
+    backgroundColor:"white"
+  },
+  tabText:{
+    color:"black"
+  }
+})
+
 ProfilScreen.navigationOptions = {
   header: null,
 };
+
