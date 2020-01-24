d0bf2d8f7ee4577f84396baa7ae62b649d454f8e
Bérénice Boulade <b.berenice@orange.fr>
2020-01-24 18:42:06 +0100

    :sparkles: Ajout de la carte Google Map

--- screens/CarteScreen.js
+++ screens/CarteScreen.js
@@ -1,42 +1,63 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
-import { Text, Content, Thumbnail, Button, Container, Header, Item, Icon, Input, Card } from 'native-base';
+import { Text, Content, Thumbnail, Button, Container, Header, Item, Icon, Input, Card, View } from 'native-base';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+
 
 import events from '../data/eventData'
 import EventItem from '../components/EventItem'
 
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors'  
 
 class CarteScreen extends React.Component {
   constructor(props){
     super(props)
   }
-  _displayDetailEvent = (idEvent, titleEvent, typeEvent) => {
+  _displayDetailEvent = (idEvent, titleEvent, typeEvent, dureeEvent, parcoursEvent, dateEvent) => {
     console.log(idEvent, titleEvent)
-    this.props.navigation.navigate("EventDetail" , { idEvent: idEvent, titleEvent: titleEvent, typeEvent :typeEvent })
+    this.props.navigation.navigate("EventDetail" , { idEvent: idEvent, titleEvent: titleEvent, typeEvent :typeEvent, dureeEvent: dureeEvent, parcoursEvent: parcoursEvent, dateEvent: dateEvent })
   }
   render(){
   let {height, width} = Dimensions.get('window');   
   return (
     <Content>
 
-      <Card style={styles.searchBar}>
+      <Card style={styles.searchBar} transparent>
           <Item>
             <Input />
             <Button style={{backgroundColor:Colors.tintColor}}>
               <Text>Chercher</Text>
             </Button>
           </Item>
-    </Card>
-      <Content>
+      </Card>
+
+      {/* <Content>
       <FlatList
         data={events}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({item}) => <EventItem event={item} displayDetailEvent={this._displayDetailEvent}/>}  
         />
-      </Content>
+      </Content> */}
       
-      <Image style={[styles.backgroundImage, {height:height, width: width}]} source={require('../assets/images/fakemap.png')}/>
+      {/* <Image style={[styles.backgroundImage, {height:height, width: width}]} source={require('../assets/images/fakemap.png')}/> */}
+
+     
+      <MapView 
+        style={{flex:1, height:height, width:width}}
+        initialRegion={{
+          latitude: 43.124830, 
+          longitude: 5.928624,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.03,
+        }}>
+        <Marker 
+          coordinate={{latitude: 43.124830, longitude: 5.928624}} 
+          description={'Test'}
+          title={'TEst'}
+        />
+      </MapView> 
+      
     </Content>
   );
 }
