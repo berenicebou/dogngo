e5868220548da2fe678c4ad722e8474295bd0977
Bérénice Boulade <b.berenice@orange.fr>
2020-01-25 23:30:45 +0100

    :sparkles: Carte dynamique des events avec marqueurs bleue clickable direct vers les details

--- data/eventData.js
+++ data/eventData.js
@@ -8,6 +8,9 @@ export default data = [
        duree: "20",
        date: "15/02/20",
        parcours: "2.3",
+       latitude:43.161260,
+       longitude:6.067227,
+       icon:require('../assets/images/image-44.png')
     },
     {
       id:2,
@@ -16,22 +19,32 @@ export default data = [
       duree: "35",
       date: "25/01/20",
       parcours: "2.4",
+      latitude:43.183259,
+      longitude:5.899638,
+      icon:require('../assets/images/image-44.png')
    },
    {
-     id:3,
-     title:"Mont Faron",
-     type : "Balades",
-     duree: "40",
+      id:3,
+      title:"Parc de la tour royale",
+      type : "Balades",
+      duree: "40",
       date: "02/03/20",
-      parcours: "3",
+      parcours: "3",   
+      latitude:3.105255,
+      longitude:5.927721,
+      icon:require('../assets/images/image-44.png')
   },
   {
     id:4,
     title:"Salon du chiot de Toulon",
     type: "Salon",
     duree: "40",
-      date: "02/03/20",
-      parcours: "3",
+    date: "02/03/20",
+    parcours: "3",
+    latitude:43.124830,
+    longitude:5.928624,
+    icon:require('../assets/images/image-45.png')
+
     }
  ] 
  
