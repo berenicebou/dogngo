18f26b0bbca09405795c4d3ca9c54bb9ac51f096
Bérénice Boulade <b.berenice@orange.fr>
2020-01-28 22:41:15 +0100

    :art: Amelioration de la page Home

--- components/NewItem.js
+++ components/NewItem.js
@@ -3,7 +3,7 @@
 import React from 'react'
 
 import { StyleSheet, View, Image } from 'react-native'
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, Col, Grid, Row } from 'native-base';
 import Colors from '../constants/Colors';
 
 class NewItem extends React.Component {
@@ -11,31 +11,28 @@ class NewItem extends React.Component {
     const news = this.props.new
     return (
         <Content>
-          <Card transparent style={{marginBottom:-15}}>
-          <CardItem>
+          <Card transparent style={{marginBottom:-30}}>
+          <CardItem style={{paddingTop:2}}>
               <Left>
                 <Thumbnail source={news.profile_photo} />
                 <Body>
                   <Text>{news.pseudo}</Text>
-                  <Text note>hier à 21:51</Text>
+                  <Text note>Il y a {news.release_date}</Text>
                 </Body>
               </Left>
             </CardItem>
             <CardItem cardBody style={{margin:-3}}>
-              <Image source={news.photo} style={{height: 250, width: null, flex: 1}}/>
+              <Image source={news.photo} style={{height: 200, width: null, flex: 1}}/>
             </CardItem>
-            <CardItem style={{height:80}}>
-              <Left>               
-              </Left>
-              <Body>
-              </Body>
-              <Right>
-                <Button transparent>
-                  <Icon active type="MaterialCommunityIcons" color={Colors.tintColor} name="dog"/>
-                  <Text style={{color:Colors.tintColor}}>12</Text>
-                </Button>
-              </Right>
-            </CardItem>
+            <Grid style={{flex:1, height:60, alignItems:'flex-end', zIndex:5}}>
+              <Row style={{margin:5, marginTop:-19, alignItems:'center'}}>
+              {news.avatarLikes ? (
+                <Icon active type="AntDesign" style={{color:Colors.tintColor}} name="heart"/>
+              ): <Icon active type="AntDesign" style={{color:Colors.tintColor}} name="hearto"/> }
+
+                <Text style={{color:Colors.tintColor, marginLeft:5}}>{news.likes}</Text>
+              </Row>
+            </Grid>
           </Card>
         </Content>
     )
